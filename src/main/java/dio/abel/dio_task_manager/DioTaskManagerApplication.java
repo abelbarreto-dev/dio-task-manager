818661529171afa4a1cd9fc0b2af516272b7a35e
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-23 22:44:30 -0300

    feat: main menu project

--- src/main/java/dio/abel/dio_task_manager/DioTaskManagerApplication.java
+++ src/main/java/dio/abel/dio_task_manager/DioTaskManagerApplication.java
@@ -1,13 +1,49 @@
 package dio.abel.dio_task_manager;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DioTaskManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DioTaskManagerApplication.class, args);
+		var scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Board Task Manager");
+			System.out.println("1. Create New Board");
+			System.out.println("2. Select Board");
+			System.out.println("3. Delete Board");
+			System.out.println("4. Log Out");
+			System.out.println("Your Alternative?");
+			String alternative = scanner.nextLine();
+
+			if (!alternative.matches("^[0-9]+$"))
+				continue;
+
+			int choice = Integer.parseInt(alternative);
+
+			switch (choice) {
+				case 1: {
+					System.out.println("Creating New Board");
+					break;
+				}
+				case 2: {
+					System.out.println("Selecting Board");
+					break;
+				}
+				case 3: {
+					System.out.println("Deleting Board");
+					break;
+				}
+				case 4: {
+					System.out.println("Log outing...");
+					return;
+				}
+				default:
+					System.out.println("Invalid alternative");
+			}
+		}
 	}
 
 }
