03b4b98b0df981762045833cecd4713833782a76
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-23 22:22:16 -0300

    feat: project created

---
+++ src/main/java/dio/abel/dio_task_manager/DioTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package dio.abel.dio_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/abel/dio_task_manager/DioTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.abel.dio_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
